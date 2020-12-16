bdf9fbbae108806036b5205776346239bf4b7017
RajapandiR <rajapandibsc12@gmail.com>
2020-12-16 13:14:54 +0530

    bug fixed

--- app.js
+++ app.js
@@ -15,46 +15,46 @@ import { DB, IN_PORD, PORT, SESSION_SECRET } from './config';
 const app = express()
 const MongoStore = connectMongo(session);
 app.use(cors());
-app.use(session({
-  resave: false,
-  saveUninitialized: true,
-  keys: ['username', 'token' , "id" , "role", "isValid"],
-  secret: SESSION_SECRET,
-  cookie: { maxAge: 1209600000 }, // two weeks in milliseconds
-  store: new MongoStore({
-    url: DB,
-    autoReconnect: true,
-    autoRemove: 'disabled'
-  })
-}));
+// app.use(session({
+//   resave: false,
+//   saveUninitialized: true,
+//   keys: ['username', 'token' , "id" , "role", "isValid"],
+//   secret: SESSION_SECRET,
+//   cookie: { maxAge: 1209600000 }, // two weeks in milliseconds
+//   store: new MongoStore({
+//     url: DB,
+//     autoReconnect: true,
+//     autoRemove: 'disabled'
+//   })
+// }));
 
-const server = new ApolloServer({
-	root,
-	typeDefs,
-  resolvers,
+// const server = new ApolloServer({
+// 	root,
+// 	typeDefs,
+//   resolvers,
 
-  formatError(err) {
-  //   let s=err.message.split('/')
-  //   console.log(s) 
-  //   // console.log(err.message.split('/'))
-  //   // const error = getErrorCode(err.message)
-    return ({
-      message: err.message
-    })
-  },
-  playground: IN_PORD
-    ? false
-    : {
-        settings: {
-          "request.credentials": "include",
-        },
-      },
-  context: ({ req, res }) => ({ req, res }),
-  uploads: {
-    maxFileSize: 10000000, // 10 MB
-    maxFiles: 20
-  }
-});
+//   formatError(err) {
+//   //   let s=err.message.split('/')
+//   //   console.log(s) 
+//   //   // console.log(err.message.split('/'))
+//   //   // const error = getErrorCode(err.message)
+//     return ({
+//       message: err.message
+//     })
+//   },
+//   playground: IN_PORD
+//     ? false
+//     : {
+//         settings: {
+//           "request.credentials": "include",
+//         },
+//       },
+//   context: ({ req, res }) => ({ req, res }),
+//   uploads: {
+//     maxFileSize: 10000000, // 10 MB
+//     maxFiles: 20
+//   }
+// });
 
 // mongoose.connect(DB,
 // 	{
@@ -65,8 +65,8 @@ const server = new ApolloServer({
 //   });
 
 app.get('/', (req, res) => res.send("hello world"))
-server.applyMiddleware({ app, cors: false });
-app.listen({port: PORT},() => console.log(`Apollo Server start on localhost:${PORT}${server.graphqlPath}`))
+// server.applyMiddleware({ app, cors: false });
+app.listen({port: PORT},() => console.log(`Apollo Server start on localhost:${PORT}`))
 
 
 // import express from 'express';
