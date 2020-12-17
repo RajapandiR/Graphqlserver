02e3d4e511b96b397be9795b2131d7730865a7b7
RajapandiR <rajapandibsc12@gmail.com>
2020-12-17 13:00:35 +0530

    bug fix

--- app.js
+++ app.js
@@ -57,15 +57,15 @@ const server = new ApolloServer({
     maxFiles: 20
   }
 });
-const MONGODB_URI = "mongodb+srv://Pandi:pandian12@cluster0.h3zrn.mongodb.net/graphql?retryWrites=true&w=majority";
+// const MONGODB_URI = "mongodb+srv://Pandi:pandian12@cluster0.h3zrn.mongodb.net/graphql?retryWrites=true&w=majority";
 // heroku config:set MONGODB_URI="mongodb+srv://Pandi:pandian12@cluster0.h3zrn.mongodb.net/graphql?retryWrites=true&w=majority"
-// mongoose.connect(DB || process.env.MONGODB_URI,
-// {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       useCreateIndex:true,
-//       useFindAndModify: false
-//     });
+mongoose.connect(DB || process.env.MONGODB_URI,
+{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex:true,
+      useFindAndModify: false
+    });
 // const DB_URL = "mongodb+srv://Pandi:pandian12@cluster0-h3zrn.mongodb.net/graphql?retryWrites=true&w=majority"
 // mongoose.connect(DB_URL,
 // 	{
